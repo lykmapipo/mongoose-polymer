189f7ac990bcf9ec47ecf5e88a3594d83ddbf546
lykmapipo <lallyelias87@gmail.com>
2015-05-05 12:05:12 +0300

    review codes and adding inline code comments

--- index.js
+++ index.js
@@ -10,42 +10,91 @@ var ObjectId = Schema.Types.ObjectId;
 //------------------------------------------------------------------------------
 // polymer utilities
 //------------------------------------------------------------------------------
+//
+//
+/**
+ * @function
+ * @description compute name of the owning model of the polymorphic association
+ * @return {String} valid registered mongoose model name
+ * @private
+ */
 function getMorpByModelName() {
+    //this refer to the model instance context
+    //
     /*jshint validthis:true*/
     return inflection.classify(inflection.singularize(this.collection.name));
 }
 
+/**
+ * @function
+ * @description computing owning side polymorphic attributes (i.e id and type)
+ *              and join them to form valid mongoose criteria to find the model
+ *              instance that owning polymorphism.  
+ * @param  {String} morphName polymorphic name
+ * @return {Object}           a valid mongoose criteria that can be used to find
+ *                            owner instance of polymorphism
+ *
+ * @private
+ */
 function buildMorpCriteria(morphName) {
+    //this refer to model instance context
+    //
     /*jshint validthis:true*/
+    //compute polymorphism owner model name
     var morphBy = getMorpByModelName.call(this);
 
     var criteria = {};
+
+    //build polymorphism id by joining morpName and owner id
+    //
+    //Example of this is when you have `imageable` polymorphism,
+    //then the id criteria will add imageableId into criteria
+    //which use owning model instance _id as value
     criteria[morphName + 'Id'] = this._id;
+
+    //build polymorphism type by joining morpName and owner model name
+    //
+    //Example of this is when you have imageable polymorphism
+    //then the type criteria will add imageableType into criteria
+    //which use owning model name as value
     criteria[morphName + 'Type'] = morphBy;
 
     return criteria;
 }
 
 //------------------------------------------------------------------------------
-//add morphBy to Schema
+//morphBy patches
 //------------------------------------------------------------------------------
+/**
+ * @function
+ * @description Build polymorphism owning side model, set up required attributes
+ *              and adding polymorphism owning side finders
+ * @param  {String} modelName valid mongoose model name that owning this polymorphism
+ * @param  {String} morphName name of the formed polymorphism
+ * @public
+ */
 Schema.prototype.morphBy = function(modelName, morphName) {
+    //check fo model name
     if (!modelName) {
         throw new Error('No model name provided');
     }
 
+    //check for polymorphism name
     if (!morphName) {
         throw new Error('No polymorphic name provided');
     }
 
-    //prepare morph schema fields
+    //prepare polymorphism schema fields
     var fields = {};
+
+    //prepare polymorphism id field
     var morphIdField = morphName + 'Id';
     fields[morphIdField] = {
         type: ObjectId,
         index: true
     };
 
+    //prepare polymorphism type field
     var morphTypeField = morphName + 'Type';
     fields[morphTypeField] = {
         type: String,
@@ -54,16 +103,26 @@ Schema.prototype.morphBy = function(modelName, morphName) {
 
     this.add(fields);
 
-    //add morphBy finder
+    //adding polymorphism owner finder
     this.methods[morphName] = function(callback) {
+        //prepare polymorphism owner id criteria
+        //
+        //TODO review if this enough criteria to find
+        //the owning model instance?
+        //
         var criteria = {};
         criteria._id = this[morphIdField];
 
+        //build owner finder query
         var query = mongoose.model(modelName).find(criteria);
 
+        //if callback provided execute query immediately
         if (_.isFunction(callback)) {
             return query.exec(callback);
-        } else {
+        }
+
+        //otherwise return mongoose query
+        else {
             return query;
         }
     };
